1df909fc4d8257c3e831ba86831de06b873a7170
Zechr <zecharixs@gmail.com>
2019-04-07 11:42:50 -0700

    AliceStar: Remove duplicate response in yugioh module

--- AliceStar/modules/yugioh.js
+++ AliceStar/modules/yugioh.js
@@ -3,8 +3,7 @@ function yugiohCom(words, topicClass) {
   if (index > - 1) {
     words = words.splice(index + 1);
   }
-  topicResp(topicClass);
-  var link = "http://shop.tcgplayer.com/productcatalog/product/show?newSearch=false&ProductName=";
+  var link = "https://shop.tcgplayer.com/productcatalog/product/show?newSearch=false&ProductName=";
   paramSearch(link, words, "%20", "", "TCG Player");
   var link2 = "https://www.reddit.com/r/yugioh/search?q=";
   paramSearch(link2, words, "+", "&restrict_sr=on&sort=relevance&t=all", "Reddit")
