4849ecec8d738307747406b0c03d6f0b55144c6d
Zechr <zecharixs@gmail.com>
2023-12-10 10:42:24 -0800

    Deprecating

--- AliceStar/training/compile.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-python topicmodel.py manifest_topics.json ../module_list.txt
-python poslabels.py manifest_labels.json
-python compile_responses.py ../module_list.txt
-python compile_commands.py ../module_list.txt
